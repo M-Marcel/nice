df0274345e81a64b710448a35e7406ce08db8cee
dannychrisa1 <dannychris.tk@gmail.com>
2024-12-08 02:10:22 +0100

    scrol-fiixed

--- src/components/Modals.tsx
+++ src/components/Modals.tsx
@@ -1,73 +1,76 @@
-import { useEffect } from "react"
-import { useModal } from "../context/ModalContext"
-import ForgotPassword from "../pages/auth/forgot-password"
-import Login from "../pages/auth/login"
-import ResetPassword from "../pages/auth/reset-password"
-import SignUp from "../pages/auth/signup"
-import ValidateOtp from "../pages/auth/validate-otp"
-import Modal from "./Modal"
-import PasswordChangeSuccess from "./PasswordChangeSuccess"
+import { useModal } from "../context/ModalContext";
+import ForgotPassword from "../pages/auth/forgot-password";
+import Login from "../pages/auth/login";
+import ResetPassword from "../pages/auth/reset-password";
+import SignUp from "../pages/auth/signup";
+import ValidateOtp from "../pages/auth/validate-otp";
+import Modal from "./Modal";
+import PasswordChangeSuccess from "./PasswordChangeSuccess";
+import { enablePageScroll } from "scroll-lock"
 
 
 type ModalsProps = {
-    email:string
-    setEmail: React.Dispatch<React.SetStateAction<string>>;
-    // onSignupComplete: () => void;
-}
+  email: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+};
 
-const Modals = ({email, setEmail}:ModalsProps) => {
-    const { activeModal, setActiveModal } = useModal()
-   
-    useEffect(() => {
-        const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+const Modals = ({ email, setEmail }: ModalsProps) => {
+  const { activeModal, setActiveModal } = useModal();
 
-        if (activeModal) {
-            document.body.style.overflow = "hidden";
-            document.body.style.paddingRight = `${scrollbarWidth}px`;
-        } else {
-            document.body.style.overflow = "auto";
-            document.body.style.paddingRight = "0";
-        }
+  const closeModal = () => {
+    setActiveModal(null);
+    document.body.style.overflow = "auto"; 
+    enablePageScroll()
 
-        return () => {
-           
-            document.body.style.overflow = "auto";
-            document.body.style.paddingRight = "0";
-        };
-    }, [activeModal]);
+  };
 
+  return (
+    <>
+      {/* Signup Modal */}
+      <Modal isVisible={activeModal === "signup"} onClose={closeModal}>
+        <SignUp />
+      </Modal>
 
-    return(
-        <>
-             <Modal isVisible={activeModal === "signup"} onClose={() => setActiveModal(null)}>
-                <SignUp />
-            </Modal>
-            <Modal isVisible={activeModal === "login"} onClose={() => setActiveModal(null)}>
-                <Login openForgotPasswordModal={() => setActiveModal("forgotPassword")} />
-            </Modal>
-            <Modal isVisible={activeModal === "forgotPassword"} onClose={() => setActiveModal(null)}>
-                <ForgotPassword
-                    openValidateOtpModal={() => setActiveModal("validateOtp")}
-                    setEmail={setEmail}
-                />
-            </Modal>
-            <Modal isVisible={activeModal === "validateOtp"} onClose={() => setActiveModal(null)}>
-                <ValidateOtp openResetPasswordModal={() => setActiveModal("resetPassword")} email={email} />
-            </Modal>
-            <Modal isVisible={activeModal === "resetPassword"} onClose={() => setActiveModal(null)}>
-                <ResetPassword
-                    email={email}
-                    openPasswordChangeSuccessModal={() => setActiveModal("passwordChangeSuccess")}
-                />
-            </Modal>
-            <Modal isVisible={activeModal === "passwordChangeSuccess"} onClose={() => setActiveModal(null)}>
-                <PasswordChangeSuccess
-                    openLoginModal={() => setActiveModal("login")}
-                />
-            </Modal>
-        </>
-       
-    )
-}
+      {/* Login Modal */}
+      <Modal isVisible={activeModal === "login"} onClose={closeModal}>
+        <Login openForgotPasswordModal={() => setActiveModal("forgotPassword")} />
+      </Modal>
 
-export default Modals
+      {/* Forgot Password Modal */}
+      <Modal isVisible={activeModal === "forgotPassword"} onClose={closeModal}>
+        <ForgotPassword
+          openValidateOtpModal={() => setActiveModal("validateOtp")}
+          setEmail={setEmail}
+        />
+      </Modal>
+
+      {/* Validate OTP Modal */}
+      <Modal isVisible={activeModal === "validateOtp"} onClose={closeModal}>
+        <ValidateOtp
+          openResetPasswordModal={() => setActiveModal("resetPassword")}
+          email={email}
+        />
+      </Modal>
+
+      {/* Reset Password Modal */}
+      <Modal isVisible={activeModal === "resetPassword"} onClose={closeModal}>
+        <ResetPassword
+          email={email}
+          openPasswordChangeSuccessModal={() =>
+            setActiveModal("passwordChangeSuccess")
+          }
+        />
+      </Modal>
+
+      {/* Password Change Success Modal */}
+      <Modal
+        isVisible={activeModal === "passwordChangeSuccess"}
+        onClose={closeModal}
+      >
+        <PasswordChangeSuccess openLoginModal={() => setActiveModal("login")} />
+      </Modal>
+    </>
+  );
+};
+
+export default Modals;
