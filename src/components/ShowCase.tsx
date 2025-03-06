3fb2f5e02f95c1c187c2f73d9a608911cf047a1d
dannychrisa1 <dannychris.tk@gmail.com>
2025-03-06 21:42:01 +0100

    made adjustments on landing-ui

--- src/components/ShowCase.tsx
+++ src/components/ShowCase.tsx
@@ -2,8 +2,8 @@ import portfolioImg from "../assets/portfolio-frame.png";
 
 const ShowCase = () => {
   return (
-      <div className="flex flex-col max-w-xl items-center text-center justify-center pt-8 ">
-        <h2 className="font-title mb-3 text-3xl lg:text-4xl text-black-300 leading-none text-center font-medium  ">
+      <div className="flex flex-col items-center text-center justify-center pt-8 ">
+        <h2 className="font-title mb-3 text-xl w-[70%] lg-w-[100%] lg:text-4xl text-black-300 leading-[25px] text-center font-medium ">
           Showcase your work with beautifully crafted templates.
         </h2>
         <img src={portfolioImg} alt="" className="pt-5  " />
--- src/components/communityBotPlan copy.tsx
+++ src/components/communityBotPlan copy.tsx
@@ -3,11 +3,11 @@ import BotSlide from "./botSlide"
 
 const CommunityBotPlan = () => {
     return (
-        <div className="relative top-[290px] lg:top-[480px]">
+        <div className="relative top-[340px] lg:top-[480px]">
             <div className="flex flex-col">
                 <div className="flex justify-center items-center flex-col text-center pb-6">
-                    <h2 className="text-4xl lg:text-4xl mb-3 text-black-300 font-medium lg:max-w-[22%]">Community bots and mini-apps</h2>
-                    <p className="text-black-400 text-md text-center w-auto lg:w-[20%]">Get access to all published projects and mini-apps</p>
+                    <h2 className="text-2xl max-w-[60%] lg:text-4xl mb-3 text-black-300 font-medium lg:max-w-[22%]">Community bots and mini-apps</h2>
+                    <p className="text-black-400 text-sm w-[60%] text-center  lg:w-[20%]">Get access to all published projects and mini-apps</p>
                 </div>
                 <div className="px-5 lg:px-7.5 xl:px-10 lg:mx-8">
                    <BotSlide />
