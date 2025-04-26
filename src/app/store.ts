15f57f87ccbfca0307ddbc4baa0470abf8bb9395
M-Marcel <39571328+M-Marcel@users.noreply.github.com>
2025-04-26 13:41:43 +0100

    Update store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -25,7 +25,7 @@ const store = configureStore({
      contact:contactSliceReducer,
     }
 })
-// updated new again
+// updated new again and again
 
 export default store
 export type RootState = ReturnType<typeof store.getState>
